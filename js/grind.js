4d044a443c5523ab3910a98809dc10f2846b2a24
Rytrex <donvo1994@gmail.com>
2016-08-30 18:50:06 -0500

    Change Text on Page

--- js/grind.js
+++ js/grind.js
@@ -21,40 +21,40 @@ function calcMG(){
 	var r5 = grind(dAmt, dCdy, 49);
 	//Set up result box before displaying
 	if(r0 < 0){
-		document.getElementById('caterpie').innerHTML = '<div style="color:red; font-weight:bold;">Catch </div>' + (-1 * r0);
+		document.getElementById('caterpie').innerHTML = '<div style="color:red; font-weight:bold; font-size:17px;">Catch </div>' + (-1 * r0);
 	}
 	else{
-		document.getElementById('caterpie').innerHTML = '<div style="color:lightgreen; font-style:italic;">Transfer </div>' + (-1 * r0);
+		document.getElementById('caterpie').innerHTML = '<div style="color:lightgreen; font-style:italic; font-size:17px;">Transfer </div>' + (-1 * r0);
 	}
 	if(r1 < 0){
-		document.getElementById('weedle').innerHTML = '<div style="color:red; font-weight:bold;">Catch </div>' + (-1 * r1);
+		document.getElementById('weedle').innerHTML = '<div style="color:red; font-weight:bold; font-size:17px;">Catch </div>' + (-1 * r1);
 	}
 	else{
-		document.getElementById('weedle').innerHTML = '<div style="color:lightgreen; font-style:italic;">Transfer </div>' + r1;
+		document.getElementById('weedle').innerHTML = '<div style="color:lightgreen; font-style:italic; font-size:17px;">Transfer </div>' + r1;
 	}
 	if(r2 < 0){
-		document.getElementById('pidgey').innerHTML = '<div style="color:red; font-weight:bold;">Catch </div>' + (-1 * r2);
+		document.getElementById('pidgey').innerHTML = '<div style="color:red; font-weight:bold; font-size:17px;">Catch </div>' + (-1 * r2);
 	}
 	else{
-		document.getElementById('pidgey').innerHTML = '<div style="color:lightgreen; font-style:italic;">Transfer </div>' + r2;
+		document.getElementById('pidgey').innerHTML = '<div style="color:lightgreen; font-style:italic; font-size:17px;">Transfer </div>' + r2;
 	}
 	if(r3 < 0){
-		document.getElementById('rattata').innerHTML = '<div style="color:red; font-weight:bold;">Catch </div>' + (-1 * r3);
+		document.getElementById('rattata').innerHTML = '<div style="color:red; font-weight:bold; font-size:17px;">Catch </div>' + (-1 * r3);
 	}
 	else{
-		document.getElementById('rattata').innerHTML = '<div style="color:lightgreen; font-style:italic;">Transfer </div>' + r3;
+		document.getElementById('rattata').innerHTML = '<div style="color:lightgreen; font-style:italic; font-size:17px;">Transfer </div>' + r3;
 	}
 	if(r4 < 0){
-		document.getElementById('zubat').innerHTML = '<div style="color:red; font-weight:bold;">Catch </div>' + (-1 * r4);
+		document.getElementById('zubat').innerHTML = '<div style="color:red; font-weight:bold; font-size:17px;">Catch </div>' + (-1 * r4);
 	}
 	else{
-		document.getElementById('zubat').innerHTML = '<div style="color:lightgreen; font-style:italic;">Transfer </div>' + r4;
+		document.getElementById('zubat').innerHTML = '<div style="color:lightgreen; font-style:italic; font-size:17px;">Transfer </div>' + r4;
 	}
 	if(r5 < 0){
-		document.getElementById('drowzee').innerHTML = '<div style="color:red; font-weight:bold;">Catch </div>' + (-1 * r5);
+		document.getElementById('drowzee').innerHTML = '<div style="color:red; font-weight:bold; font-size:17px;">Catch </div>' + (-1 * r5);
 	}
 	else{
-		document.getElementById('drowzee').innerHTML = '<div style="color:lightgreen; font-style:italic;">Transfer </div>' + r5;
+		document.getElementById('drowzee').innerHTML = '<div style="color:lightgreen; font-style:italic; font-size:17px;">Transfer </div>' + r5;
 	}
 	//Display result box
 	document.getElementById('result').style.display = "block";
