5efe361efa5e5ed62daf30e16ed250efc3ba6aa8
Rytrex <donvo1994@gmail.com>
2016-08-30 19:47:43 -0500

    New Calc and Changed Animations
    Added 'Transfer' calculator that needs to be tweaked.

--- js/grind.js
+++ js/grind.js
@@ -1,4 +1,4 @@
-function calcMG(){
+function calcMG(cal){
 	//Get user submitted information
 	var cAmt = document.getElementById('cAmt').value;
 	var cCdy = document.getElementById('cCdy').value;
@@ -13,12 +13,22 @@ function calcMG(){
 	var dAmt = document.getElementById('dAmt').value;
 	var dCdy = document.getElementById('dCdy').value;
 	//Calculate the recommended amounts
-	var r0 = grind(cAmt, cCdy, 11);
-	var r1 = grind(wAmt, wCdy, 11);
-	var r2 = grind(pAmt, pCdy, 11);
-	var r3 = grind(rAmt, rCdy, 24);
-	var r4 = grind(zAmt, zCdy, 49);
-	var r5 = grind(dAmt, dCdy, 49);
+	if(cal == 0){
+		var r0 = grind(cAmt, cCdy, 11, 1);
+		var r1 = grind(wAmt, wCdy, 11, 1);
+		var r2 = grind(pAmt, pCdy, 11, 1);
+		var r3 = grind(rAmt, rCdy, 24, 1);
+		var r4 = grind(zAmt, zCdy, 49, 1);
+		var r5 = grind(dAmt, dCdy, 49, 1);
+	}
+	else if(cal == 1){
+		var r0 = grind(cAmt, cCdy, 10, 2);
+		var r1 = grind(wAmt, wCdy, 10, 2);
+		var r2 = grind(pAmt, pCdy, 10, 2);
+		var r3 = grind(rAmt, rCdy, 23, 2);
+		var r4 = grind(zAmt, zCdy, 48, 2);
+		var r5 = grind(dAmt, dCdy, 48, 2);
+	}
 	//Set up result box before displaying
 	if(r0 < 0){
 		document.getElementById('caterpie').innerHTML = '<div style="color:red; font-weight:bold; font-size:17px;">Catch </div>' + (-1 * r0);
@@ -61,9 +71,20 @@ function calcMG(){
 	
 }
 
-function grind(amt, cdy, evo){
+function grind(amt, cdy, evo, add){
 	if(amt == 0 && cdy == 0) return 0;
-	var hold = (cdy - ((amt * evo) + 1))/3;
+	var hold = (cdy - ((amt * evo) + add))/3;
 	if(hold < 0) return Math.floor(hold);
 	else return Math.ceil(hold);
+}
+
+function sBut(cho){
+	if(cho == 0){
+		document.getElementById('tran').style.display = "none";
+		document.getElementById('norm').style.display = "block";
+	}
+	if(cho == 1){
+		document.getElementById('norm').style.display = "none";
+		document.getElementById('tran').style.display = "block";
+	}
 }
